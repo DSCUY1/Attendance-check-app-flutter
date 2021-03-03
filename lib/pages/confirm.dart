3d0e5c7f57ca9b5ec58fbe1bcf7415cbb86144b9
Guiffou-Joël <guiffoujoel@gmail.com>
2021-03-03 17:24:37 +0100

    Front confirm page OK

--- lib/pages/confirm.dart
+++ lib/pages/confirm.dart
@@ -11,36 +11,47 @@ class _ConfirmState extends State<Confirm> {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-        body: Center(
-            child: Column(children: <Widget>[
-      Container(
-        margin: EdgeInsets.only(top: 150),
-        child: Text(
-          "Confirmed",
-          style: TextStyle(
-              fontFamily: "Roboto", fontSize: 30, fontWeight: FontWeight.w400),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 150),
+              child: Text(
+                "Confirmed",
+                style: TextStyle(
+                  fontFamily: "Roboto",
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              width: width * 0.5,
+              child: Image.asset('assets/images/validation.png'),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              child: RaisedButton(
+                color: Color(0xff6A4E77),
+                textColor: Colors.white,
+                onPressed: () {
+                  MaterialPageRoute route =
+                      MaterialPageRoute(builder: (_) => Home());
+                  Navigator.push(context, route);
+                },
+                child: Text(
+                  "+ HOME",
+                  style: TextStyle(
+                      fontFamily: "Roboto",
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-      Container(
-          margin: EdgeInsets.only(top: 50),
-          width: width * 0.5,
-          child: Image.asset('assets/images/validation.png')),
-      Container(
-          margin: EdgeInsets.only(top: 50),
-          child: RaisedButton(
-            onPressed: () {
-              MaterialPageRoute route =
-                  MaterialPageRoute(builder: (_) => Home());
-              Navigator.push(context, route);
-            },
-            child: Text(
-              "+ HOME",
-              style: TextStyle(
-                  fontFamily: "Roboto",
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20),
-            ),
-          ))
-    ])));
+    );
   }
 }
