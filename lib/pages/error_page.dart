e6202f970a66b6b507919c6a1a5cedb79b2d551a
Guiffou-Joël <guiffoujoel@gmail.com>
2021-03-03 17:19:03 +0100

    Front error page OK

--- lib/pages/error_page.dart
+++ lib/pages/error_page.dart
@@ -9,35 +9,49 @@ class ErrorPage extends StatefulWidget {
 class _ErrorPageState extends State<ErrorPage> {
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Column(children: <Widget>[
-      Container(
-        margin: EdgeInsets.only(top: 150),
-        child: Text(
-          "Not recognized",
-          style: TextStyle(
-              fontFamily: "Roboto", fontSize: 30, fontWeight: FontWeight.w400),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 150),
+              child: Text(
+                "Not recognized",
+                style: TextStyle(
+                  fontFamily: "Roboto",
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              child: Image.asset(
+                'assets/images/warning.png',
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              child: RaisedButton(
+                color: Color(0xff6A4E77),
+                textColor: Colors.white,
+                onPressed: () {
+                  MaterialPageRoute route =
+                      MaterialPageRoute(builder: (_) => Home());
+                  Navigator.push(context, route);
+                },
+                child: Text(
+                  "+ HOME",
+                  style: TextStyle(
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-      Container(
-          margin: EdgeInsets.only(top: 50),
-          child: Image.asset('assets/images/warning.png')),
-      Container(
-          margin: EdgeInsets.only(top: 50),
-          child: RaisedButton(
-            onPressed: () {
-              MaterialPageRoute route =
-                  MaterialPageRoute(builder: (_) => Home());
-              Navigator.push(context, route);
-            },
-            child: Text(
-              "+ HOME",
-              style: TextStyle(
-                  fontFamily: "Roboto",
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20),
-            ),
-          ))
-    ])));
+    );
   }
 }
